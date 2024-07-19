b7c5ec0717fe05a1b1e3192f788c02788217bdd5
anubhav100rao <anubhav100rao@gmail.com>
2024-07-20 01:31:09 +0530

    updated

--- src/main/java/otherConcepts/AtomicVariable.java
+++ src/main/java/otherConcepts/AtomicVariable.java
@@ -10,14 +10,14 @@ public class AtomicVariable {
     public static void main(String[] args) {
         Thread one = new Thread(() -> {
             for (int i = 0; i < 10000; i++) {
-                //count++;
+                // count++;
                 counter.incrementAndGet();
             }
         });
 
         Thread two = new Thread(() -> {
             for (int i = 0; i < 10000; i++) {
-                //count++;
+                // count++;
                 counter.incrementAndGet();
             }
         });
